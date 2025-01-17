03594d8fea5c73dd232b00884c7346306d4ef701
Murillo <Murillo@DESKTOP-HS4TJPS>
2025-01-17 16:53:47 -0300

    Adicionar arquivos de projeto.

---
+++ EventsCEC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EventsCEC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
